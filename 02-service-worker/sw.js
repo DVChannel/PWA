e848206ab6fa649f5b4288107fce7f52076de525
Channel Gonzalez Jimenez <channelgonzalezjimenez100@gmail.com>
2022-10-02 19:22:50 -0500

    sec4_vid38_formas validas fetch

---
+++ 02-service-worker/sw.js
@@ -0,0 +1,12 @@
+
+self.addEventListener('fetch', event => {
+
+if (event.request.url.includes('.jpg')){
+ console.log(event.request.url);
+    let fotoReq = fetch(event.request);
+
+    event.respondWith(fotoReq);
+}
+
+
+});
