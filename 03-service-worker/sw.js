99a4bbf2b61a43f9c9965ec92af707c89d90877b
Channel Gonzalez Jimenez <channelgonzalezjimenez100@gmail.com>
2022-10-03 22:23:17 -0500

    sec5vid49_waitUntil

--- 03-service-worker/sw.js
+++ 03-service-worker/sw.js
@@ -4,8 +4,23 @@
 self.addEventListener('install', event => {
 
     console.log('SW: Instalando SW');
+
+
+const instalacion = new Promise( (resolve, reject) => {
+
+    setTimeout(() => {
+        console.log('SW: Instalaciones terminadas');
+        self.skipWaiting();
+        resolve();
+    }, 1000);
+
 });
 
+
+event.waitUntil( instalacion );
+
+
+});
 self.addEventListener('activate', event => {
 
     console.log('SW2: activo y listo');
