a37e7085a653aa53ae72b057c69972e26777c2e2
Channel Gonzalez Jimenez <channelgonzalezjimenez100@gmail.com>
2022-10-03 22:35:28 -0500

    sec5vid50serviceWorkerFetch

--- 03-service-worker/sw.js
+++ 03-service-worker/sw.js
@@ -25,3 +25,12 @@ self.addEventListener('activate', event => {
 
     console.log('SW2: activo y listo');
 });
+
+
+self.addEventListener('fetch', event => {
+    console.log( 'SW:', event.request.url );
+     if ( event.request.url.includes('https://reqres.in/') ) {
+         const resp = new Response(`{ ok: false, mensaje: 'ajajjajj'}`);
+         event.respondWith( resp );
+     }
+});
