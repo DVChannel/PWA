aed6601be2bf3be78c5139f1d7ddf47dbefcf3e7
Channel Gonzalez Jimenez <channelgonzalezjimenez100@gmail.com>
2022-10-27 18:26:00 -0500

    sec6vid58_introduccionCacheStorage

--- 04-cache-offline/js/app.js
+++ 04-cache-offline/js/app.js
@@ -3,3 +3,33 @@
 if ( navigator.serviceWorker ) {
     navigator.serviceWorker.register('/sw.js');
 }
+
+if (window.caches){
+    caches.open('prueba-1');
+    caches.open('prueba-2');
+
+    caches.open('cache-v1.1').then(cache =>{
+        cache.addAll([
+            '/index.html',
+            '/css/style.css',
+            '/img/main.jpg'
+        ]).then(() => {
+          //  cache.delete('/css/style.css');
+
+        });
+
+
+
+caches.keys().then(keys => {
+    console.log(keys);
+});
+
+
+
+    /*    cache.match('/index.html')
+            .then( res =>{
+                res.text().then(console.log);
+            });*/
+        });
+    
+};
