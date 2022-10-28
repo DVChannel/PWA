bd37481e6a8442087fe8ba52d8bbe9e47145e0a0
Channel Gonzalez Jimenez <channelgonzalezjimenez100@gmail.com>
2022-10-27 23:26:43 -0500

    sec7vid76cacheCnNetworkFallback

--- 06-twittor/sw.js
+++ 06-twittor/sw.js
@@ -1,4 +1,4 @@
-
+importScripts('js/sw-utils.js');
 const STATIC_CACHE    = 'static-v4';
 const DYNAMIC_CACHE   = 'dynamic-v2';
 const INMUTABLE_CACHE = 'inmutable-v1';
@@ -65,6 +65,34 @@ self.addEventListener('activate', e => {
     e.waitUntil( respuesta );
 
 });
+
+
+
+self.addEventListener( 'fetch', e => {
+
+
+    const respuesta = caches.match( e.request ).then( res => {
+
+        if ( res ) {
+            return res;
+        } else {
+
+            return fetch( e.request ).then( newRes => {
+
+                return actualizaCacheDinamico( DYNAMIC_CACHE, e.request, newRes );
+
+            });
+
+        }
+
+    });
+
+
+
+    e.respondWith( respuesta );
+
+});
+
 
 
 
