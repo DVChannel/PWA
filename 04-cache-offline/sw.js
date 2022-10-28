41966b2d556f7baeb9560b7db4a68f9a924c30c9
Channel Gonzalez Jimenez <channelgonzalezjimenez100@gmail.com>
2022-10-27 19:50:59 -0500

    sec6vid63_limitarCacheDinamico

--- 04-cache-offline/sw.js
+++ 04-cache-offline/sw.js
@@ -1,10 +1,37 @@
 
-const CACHE_NAME = 'cache-1';
+//const CACHE_NAME = 'cache-1';
+
+
+const CACHE_STATIC_NAME  = 'static-v2';
+const CACHE_DYNAMIC_NAME = 'dynamic-v1';
+const CACHE_INMUTABLE_NAME = 'inmutable-v1';
+
+
+
+function limpiarCache( cacheName, numeroItems ) {
+
+
+    caches.open( cacheName )
+        .then( cache => {
+
+            return cache.keys()
+                .then( keys => {
+                    
+                    if ( keys.length > numeroItems ) {
+                        cache.delete( keys[0] )
+                            .then( limpiarCache(cacheName, numeroItems) );
+                    }
+                });
+
+            
+        });
+}
+
 
 self.addEventListener('install', e => {
 
 
-    const cacheProm = caches.open( 'cache-1' )
+    const cacheProm = caches.open( CACHE_NAME )
         .then( cache => {
 
             return cache.addAll([
